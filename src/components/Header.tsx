f2e734be6ab5bf5edd33acbe20dfe88bcd3295eb
private-ryomann <ryomann46497@gmail.com>
2024-10-15 01:01:50 +0900

    Dom使用したテスト追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react"; // これを追加
+
+export const Header = () => {
+	return (
+		<header>
+			<p>test</p>
+		</header>
+	);
+};
---
+++ src/components/component.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from "./Header";
+
+// テスト対象のReactコンポーネント
+
+test("ページ中に「test」が表示される。", () => {
+	render(<Header />);
+	expect(screen.getByText("test")).toBeVisible();
+});
